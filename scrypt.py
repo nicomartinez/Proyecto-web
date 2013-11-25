28468f021508cf9a03e111d52f3a918390537162
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-25 16:32:05 -0500

    Create scrypt.py

---
+++ scrypt.py
@@ -0,0 +1,11 @@
+·en este archivo se definiran los metodos del manejko de la base
+import MySQLdb as db
+
+try:
+  con = db.connect("Pacientes.db")
+  con.execute("CREATE TABLE pacientes values()")
+  
+  def agregarPaciente(firstName, lastname, cc, prioridad,tiempoIngreso):
+      con.execute(""
+except:
+  print "error"
