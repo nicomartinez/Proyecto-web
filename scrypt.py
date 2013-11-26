e63418f0eafdd59e7dc845b4abaefa3319cf1ca9
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-26 14:05:31 -0500

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -1,15 +1,18 @@
 #en este archivo se definiran los metodos del manejko de la base
-import MySQLdb as db
+import sqlite3 as db
 
 try:
   con = db.connect("Pacientes.db")
-  con.execute("CREATE TABLE pacientes values()")
+  con.execute("CREATE TABLE pacientes values(fisrstName varchar(20), lastName varchar(20), cc INT, prioridad varchar(10), tiempoIngreso Timestamp)")
   
   def agregarPaciente(firstName, lastname, cc, prioridad,tiempoIngreso):
-      con.execute("")
+      con.execute("INSERT INTO pacientes values(" + fistName + ", " + lastName + ", " + cc + ", " + prioridad + ", " + tiempoIngreso + ")")
       
   def agregarPaciente(firstName, lastname, cc, prioridad,tiempoIngreso):
-      con.execute("UPDATE ")
-    
+      con.execute("UPDATE pacientes SET firstName WHERE ")
+  def borrarPaciente(name):
+      con.execute("DELETE FROM pacientes WHERE firstName = " + name)
+  def mostrarLista():
+      con.execute("SELECT * FROM pacientes")
 except:
   print "error"
