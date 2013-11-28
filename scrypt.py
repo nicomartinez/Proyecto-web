f56016ff06b2a49e0c0b88e66da7ab0f17a62c5b
nicomartinez <juannicolas.martinez@uptc.edu.co>
2013-11-27 21:05:20 -0500

    Update scrypt.py

--- scrypt.py
+++ scrypt.py
@@ -5,13 +5,15 @@ try:
   con = db.connect("Pacientes.db")
   con.execute("CREATE TABLE pacientes values(fisrstName varchar(20), lastName varchar(20), cc INT, prioridad varchar(10), tiempoIngreso Timestamp)")
   
-  def agregarPaciente(firstName, lastname, cc, prioridad,tiempoIngreso):
+  def agregarPaciente(firstName, lastName, cc, prioridad,tiempoIngreso):
       con.execute("INSERT INTO pacientes values(" + fistName + ", " + lastName + ", " + cc + ", " + prioridad + ", " + tiempoIngreso + ")")
       
-  def agregarPaciente(firstName, lastname, cc, prioridad,tiempoIngreso):
-      con.execute("UPDATE pacientes SET firstName WHERE ")
+  def agregarPaciente(firstName, lastName, cc, prioridad,tiempoIngreso):
+      con.execute("UPDATE pacientes SET firstName = " + firtName +",  lastNme = " + lastName + ", cc = " + cc + ", prioridad = " + prioridad + ", tiempoIngreso = " + tiempoIngreso)
+      con.commit()
   def borrarPaciente(name):
       con.execute("DELETE FROM pacientes WHERE firstName = " + name)
+      con.commit()
   def mostrarLista():
       con.execute("SELECT * FROM pacientes")
 except:
